f291fbb83b5e00ac5321090d051f1057a61cdbe5
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-08-18 13:48:04 -0600

    chore: fix iptables persist, add cgroupsv2

--- packer/scripts/os-prep.sh
+++ packer/scripts/os-prep.sh
@@ -2,6 +2,8 @@
 
 echo "Performing OS prep necessary for DUBBD on RKE2..."
 
+# iptables for RKE2
+apt-get install iptables-persistent -y
 iptables -A INPUT -p tcp -m tcp --dport 2379 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 2380 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 9345 -m state --state NEW -j ACCEPT
@@ -19,37 +21,33 @@ iptables -A INPUT -p udp -m udp --dport 51820 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 51821 -m state --state NEW -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables-save
+iptables-save > /etc/iptables/rules.v4
 
 echo "* soft nofile 13181250" >> /etc/security/limits.d/ulimits.conf
 echo "* hard nofile 13181250" >> /etc/security/limits.d/ulimits.conf
 echo "* soft nproc  13181250" >> /etc/security/limits.d/ulimits.conf
 echo "* hard nproc  13181250" >> /etc/security/limits.d/ulimits.conf
 
+# sysctl changes for DUBBD
 sysctl -w vm.max_map_count=524288
 echo "vm.max_map_count=524288" > /etc/sysctl.d/vm-max_map_count.conf
-
 sysctl -w fs.nr_open=13181252
 echo "fs.nr_open=13181252" > /etc/sysctl.d/fs-nr_open.conf
-
 sysctl -w fs.file-max=13181250
 echo "fs.file-max=13181250" > /etc/sysctl.d/fs-file-max.conf
-
 echo "fs.inotify.max_user_instances=1024" > /etc/sysctl.d/fs-inotify-max_user_instances.conf
 sysctl -w fs.inotify.max_user_instances=1024
-
 echo "fs.inotify.max_user_watches=1048576" > /etc/sysctl.d/fs-inotify-max_user_watches.conf
 sysctl -w fs.inotify.max_user_watches=1048576
-
 echo "vm.overcommit_memory=1" >> /etc/sysctl.d/90-kubelet.conf
 sysctl -w vm.overcommit_memory=1
 echo "kernel.panic=10" >> /etc/sysctl.d/90-kubelet.conf
 sysctl -w kernel.panic=10
 echo "kernel.panic_on_oops=1" >> /etc/sysctl.d/90-kubelet.conf
 sysctl -w kernel.panic_on_oops=1
-
 sysctl -p
 
+# modprobes for Istio
 modprobe br_netfilter
 modprobe xt_REDIRECT
 modprobe xt_owner
@@ -58,3 +56,7 @@ echo "br_netfilter" >> /etc/modules-load.d/istio-iptables.conf
 echo "xt_REDIRECT" >> /etc/modules-load.d/istio-iptables.conf
 echo "xt_owner" >> /etc/modules-load.d/istio-iptables.conf
 echo "xt_statistic" >> /etc/modules-load.d/istio-iptables.conf
+
+# cgroupsv2 for RKE2 + NeuVector
+sed -i 's/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"systemd.unified_cgroup_hierarchy=1/' /etc/default/grub
+update-grub
