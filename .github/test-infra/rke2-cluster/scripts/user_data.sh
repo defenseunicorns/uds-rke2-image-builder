a45f6c30511d8eb0f7e3c68e2b18fb8f9eb43cd3
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-10-17 17:06:56 -0600

    fix for realz

--- .github/test-infra/rke2-cluster/scripts/user_data.sh
+++ .github/test-infra/rke2-cluster/scripts/user_data.sh
@@ -7,7 +7,12 @@ else
     bootstrap_ip=${BOOTSTRAP_IP}
 fi
 
-echo "Bootstrap node IP: $${bootstrap_ip}"
+if [[ "${CLUSTER_SANS}" ]]; then
+    echo "Passing SANs to RKE2 startup script: ${CLUSTER_SANS}"
+    public_ipv4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    # Use array to properly handle cluster_sans containing multiple values
+    san_options=(-T "$${public_ipv4} ${CLUSTER_SANS}")
+fi
 
 if [[ "${AGENT_NODE}" == "true" ]]; then
     /root/rke2-startup.sh -t ${RKE2_JOIN_TOKEN} -s $${bootstrap_ip} -a
