12bb34861edee182512093def2bb26a9bbcd0596
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-10-31 11:05:41 -0600

    fix: dir locations

--- packer/scripts/rke2-startup.sh
+++ packer/scripts/rke2-startup.sh
@@ -67,23 +67,23 @@ find $dir -maxdepth 1 -type f -name "*.kubeconfig" -exec chmod 0640 {} \;
 find $dir -maxdepth 1 -type f -name "*.crt" -exec chmod 0600 {} \;
 find $dir -maxdepth 1 -type f -name "*.key" -exec chmod 0600 {} \;
 
-dir=/var/lib/rancher/rke2/agent/bin
+dir=/var/lib/rancher/rke2/bin
 chown root:root $dir/*
 chmod 0750 $dir/*
 
-dir=/var/lib/rancher/rke2/agent
-chown root:root $dir/data
-chmod 0750 $dir/data
-
 dir=/var/lib/rancher/rke2/data
+chown root:root $dir
+chmod 0750 $dir
 chown root:root $dir/*
 chmod 0640 $dir/*
 
-dir=/var/lib/rancher/rke2/server
-chown root:root $dir/*
-chmod 0700 $dir/cred
-chmod 0700 $dir/db
-chmod 0700 $dir/tls
-chmod 0751 $dir/manifests
-chmod 0750 $dir/logs
-chmod 0600 $dir/token
+if [ -z $agent ]; then
+  dir=/var/lib/rancher/rke2/server
+  chown root:root $dir/*
+  chmod 0700 $dir/cred
+  chmod 0700 $dir/db
+  chmod 0700 $dir/tls
+  chmod 0751 $dir/manifests
+  chmod 0750 $dir/logs
+  chmod 0600 $dir/token
+fi
