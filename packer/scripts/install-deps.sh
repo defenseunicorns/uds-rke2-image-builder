93fa053565db5bcfa4524084b61c4411c48ea271
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-11-13 15:45:23 -0600

    fix: add LVM for rook/ceph (#34)

--- packer/scripts/install-deps.sh
+++ packer/scripts/install-deps.sh
@@ -11,11 +11,15 @@ if [[ $DISTRO == "rhel" ]]; then
   #  Install rke2 selinux policy
   curl -LO "https://github.com/rancher/rke2-selinux/releases/download/v0.14.stable.1/rke2-selinux-0.14-1.el8.noarch.rpm"
   yum install rke2-selinux-0.14-1.el8.noarch.rpm -y
+  # Install lvm2 for storage (e.x. rook/ceph)
+  yum install lvm2 -y
 elif [[ $DISTRO == "ubuntu" ]]; then
   echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
   apt-add-repository ppa:ansible/ansible -y
   apt-get update -y && apt-get upgrade -y
   apt-get install ansible unzip jq -y
+  # Install lvm2 for storage (e.x. rook/ceph)
+  apt-get install lvm2 -y
 fi
 
 # Ensure that ansible collections needed are installed 
