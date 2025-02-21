239cce41946bc9268960204f56c85b5e3c5a6e4e
Joel McCoy <joel@defenseunicorns.com>
2025-02-21 13:34:21 -0600

    fix: update rhel8 build with new stig

--- packer/scripts/install-deps.sh
+++ packer/scripts/install-deps.sh
@@ -10,7 +10,15 @@ if [[ $DISTRO == "rhel" ]]; then
   VERSION=$( cat /etc/os-release | grep -Poi '^version="[0-9]+\.[0-9]+' | cut -d\" -f2 | cut -d. -f1 )
 
   yum update -y && yum upgrade -y
-  yum install ansible unzip nfs-utils nfs4-acl-tools lvm2 iscsi-initiator-utils -y
+  yum install unzip nfs-utils nfs4-acl-tools lvm2 iscsi-initiator-utils -y
+
+  # Install Ansible
+  # Note: Latest versions of ansible are not available in RHEL 8 or 9 repos, need to use pip
+  yum install python3.9 python3.9-pip -y # Note python3.9 is the version that currently works with rhel8 STIGs
+  python3.9 -m pip install --upgrade ansible
+  # Temporarily add /usr/local/bin to PATH to ensure ansible is available
+  export PATH=$PATH:/usr/local/bin
+
   #  Install rke2 selinux policy
   if [[ ${VERSION} -eq 9 ]] ; then
     curl -LO "https://github.com/rancher/rke2-selinux/releases/download/v0.18.stable.1/rke2-selinux-0.18-1.el9.noarch.rpm"
